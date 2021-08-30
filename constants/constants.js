67a8c47bca902e53ce6e248392c71c50ffd1f8f9
dotienanh77 <anhdt.ute@gmail.com>
2021-08-30 10:48:50 +0700

    coingeko API intergration

--- constants/constants.js
+++ constants/constants.js
@@ -5,4 +5,11 @@ const marketTabs = [
 const constants = {
   marketTabs,
 };
+//API
+// My Holdings
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc}&ids=${ids}
+
+// Coin Market
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=${orderBy}&per_page=${perPage}&page=${page}&sparkline=${sparkline}&price_change_percentage=${priceChangePerc
+
 export default constants;
