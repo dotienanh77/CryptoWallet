ccda99b7be45a2a855fb3a568e3f3da87d38e2f3
dotienanh77 <anhdt.ute@gmail.com>
2021-08-30 17:00:30 +0700

    header Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useCallback} from 'react';
 import {View, Text} from 'react-native';
 import {connect} from 'react-redux';
@@ -5,6 +6,7 @@ import {getHoldings, getCoinMarket} from '../stores/market/marketActions';
 import {MainLayout} from './';
 import {useFocusEffect} from '@react-navigation/native';
 import {FONTS, COLORS, SIZES, dummyData, icons} from '../constants';
+import {BalanceInfo} from '../components';
 const Home = ({getHoldings, getCoinMarket, myHoldings, coins}) => {
   useFocusEffect(
     useCallback(() => {
@@ -12,10 +14,35 @@ const Home = ({getHoldings, getCoinMarket, myHoldings, coins}) => {
       getCoinMarket();
     }, []),
   );
+
+  function renderWalletInforSection() {
+    return (
+      <View
+        style={{
+          paddingHorizontal: SIZES.padding,
+          borderBottomLeftRadius: 25,
+          borderBottomRightRadius: 25,
+          backgroundColor: COLORS.gray,
+        }}>
+        {/* Balance info */}
+        <BalanceInfo
+          title="Your Wallet"
+          displayAmount="45,000"
+          changePct={2.3}
+          containerStyle={{marginTop: 50}}
+        />
+        {/* Button */}
+      </View>
+    );
+  }
+
   return (
     <MainLayout>
-      <View>
-        <Text>Home screen</Text>
+      <View style={{flex: 1, backgroundColor: COLORS.black}}>
+        {/* Header Section - Wallet Information */}
+        {renderWalletInforSection()}
+        {/* Chart  */}
+        {/* Top Cryptocurrency  */}
       </View>
     </MainLayout>
   );
