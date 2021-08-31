a9050e05ecf4f73df3d084c4d92e1d852393c144
dotienanh77 <anhdt.ute@gmail.com>
2021-08-31 19:53:08 +0700

    done animated chart

--- components/Chart.js
+++ components/Chart.js
@@ -46,8 +46,65 @@ const Chart = ({containerStyle, chartPrices}) => {
     return `${date} / ${month}`;
   };
 
+  const formatNumber = (value, roundingPoint) => {
+    if (value > 1e9) {
+      return `${(value / 1e9).toFixed(roundingPoint)}B`;
+    } else if (value > 1e6) {
+      return `${(value / 1e6).toFixed(roundingPoint)}M`;
+    } else if (value > 1e3) {
+      return `${(value / 1e3).toFixed(roundingPoint)}K`;
+    } else {
+      return value.toFixed(roundingPoint);
+    }
+  };
+
+  const getAxisLabelValues = () => {
+    if (chartPrices != undefined) {
+      let minValue = Math.min(...chartPrices);
+      let maxValue = Math.max(...chartPrices);
+
+      let midValue = (minValue + maxValue) / 2;
+
+      let higherMidValue = (maxValue + midValue) / 2;
+      let lowerMidValue = (minValue + midValue) / 2;
+
+      let roundingPoint = 2;
+      return [
+        formatNumber(maxValue, roundingPoint),
+        formatNumber(lowerMidValue, roundingPoint),
+        formatNumber(higherMidValue, roundingPoint),
+        formatNumber(minValue, roundingPoint),
+      ];
+    } else {
+      return [];
+    }
+  };
+
   return (
     <View style={{...containerStyle}}>
+      {/* Y axis label */}
+      <View
+        style={{
+          position: 'absolute',
+          left: SIZES.padding,
+          top: 0,
+          bottom: 0,
+          justifyContent: 'space-between',
+        }}>
+        {/* get Y Axis Label Values */}
+        {getAxisLabelValues().map((item, index) => {
+          return (
+            <Text
+              key={index}
+              style={{
+                color: COLORS.lightGray3,
+                ...FONTS.body4,
+              }}>
+              {item}
+            </Text>
+          );
+        })}
+      </View>
       {data.length > 0 && (
         <ChartPathProvider data={{points, smoothingStrategy: 'bezier'}}>
           <ChartPath
